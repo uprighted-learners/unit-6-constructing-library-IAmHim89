77a46e6ef0cfff4f7bb00e6b29df75f9bcfc38c0
IAmHim89 <computertech0418@gmail.com>
2024-07-21 20:44:56 -0400

    updated assignment

--- library.js
+++ library.js
@@ -16,6 +16,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 //localbrowser: port#
 const myPort = process.env.port || 3001;
 
+//class library
 class Library {
   constructor(dbUrl, dbName, colName) {
     this.dbUrl = dbUrl;
@@ -23,22 +24,26 @@ class Library {
     this.colName = colName;
     this.dbClient;
   }
+  //mongo client connection
   async client() {
     console.log(`Your connected to ${this.dbUrl}`);
     this.dbClient = MongoClient.connect(this.dbUrl);
     console.log("Your Connection Is Succesful!");
     return this.dbClient;
   }
+  //testing method
   async test() {
     const client = await this.client();
     client.close();
   }
+  //async collection method
   async collection() {
     const client = await this.client();
     const db = client.db(this.dbName);
     const collection = db.collection(this.colName);
     return collection;
   }
+  //async allbooks method
   async allBooks() {
     const collection = await this.collection();
     return collection.find({});
